907c5b66395e1652d87ccba16babb0c795c03f3e
Zai <zai.alam51@gmail.com>
2025-03-22 22:11:44 +0800

    stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import Header from "@/components/core/Header";
 import Footer from "@/components/core/Footer";
-/*
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-*/
 
 export const metadata: Metadata = {
   title: "Zai Alam",
-  description: "Hello",
+  description: "",
 };
 
 export default function Layout({children}: {children: React.ReactNode }) {

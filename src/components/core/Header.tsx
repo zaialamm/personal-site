b18d5f830e859db063394b8f629f34fdd5b80bf5
Zai <zai.alam51@gmail.com>
2025-03-23 01:27:38 +0800

    stuff

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Header() {   
     return (
       <>
-      <div className="flex justify-between items-center py-8">
+      <div className="flex justify-between items-center px-5 py-8">
         <div className="text-5xl font-semibold flex justify-center">
           <Link href="/" className="">
             Z
@@ -18,7 +18,7 @@ export default function Header() {
 
       <div
         id="header-divider"
-        className="w-full h-px my-4 transform bg-gray-900 rotate-[0.5deg]"
+        className="w-sm md:w-full h-px my-4 transform bg-gray-900 rotate-[0.5deg]"
       >
 
       </div>

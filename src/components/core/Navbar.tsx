a762eb9acd86c6bc16f54c21f320799ce0c25a43
Zai <zai.alam51@gmail.com>
2025-03-23 01:29:57 +0800

    stuff

--- src/components/core/Navbar.tsx
+++ src/components/core/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav
         id = "navigation"
-        className ="flex justify-center w-full"
+        className ="flex justify-end w-full"
         >
             <ul className="flex flex-row px-6 py-2 space-x-3 tracking-wide">
                 <LinkItem href="/about" title="About" />

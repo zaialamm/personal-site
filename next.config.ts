82d68fb0ec0669269cff68f0aa39c478873eea6c
Zai <zai.alam51@gmail.com>
2025-03-22 23:59:53 +0800

    stuff

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Required for static export
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during builds
   },
   typescript: {
     ignoreBuildErrors: true, // Ignore TypeScript errors during builds
   }
-
 };
 
 export default nextConfig;
